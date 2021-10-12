dd73f4367780cf7dc828e99eea5fa77e13af12db
Daniel J. McDonald <dajmcdon@gmail.com>
2021-10-11 22:29:51 -0700

    done for the night

--- ca-trajectory.R
+++ ca-trajectory.R
@@ -18,7 +18,7 @@ ca_traj <- ggplot(pd$truth_df, mapping = aes(x = target_end_date)) +
   geom_point(data = pd$points_df,
              mapping = aes(y = value),
              color = "orange", size = 1) +
-  geom_vline(xintercept = as.Date("2020-12-28")) +
+  geom_vline(xintercept = as.Date("2020-12-28"), linetype = "dashed") +
   scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
   theme_bw(base_size = 14) + 
   theme(legend.position = "none") + 
